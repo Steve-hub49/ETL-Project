ed2e3162c8a13c4bf442ddbc4d40c09062ced3b6
Steve Freeland <stevefreeland1@gmail.com>
2020-12-30 17:12:14 -0500

    refactored column deletion

--- amazon.py
+++ amazon.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 amazon_top_100_df = pd.read_csv("top_100_books_amazon.csv", encoding='latin1')
-amazon_top_100_df = amazon_top_100_df.drop(columns=['rank2'])    # Removed duplicate and non-continous rank column
-# Rename columns from (['rank', 'book', 'author', 'rating', 'reviews', 'type', 'price'], dtype='object')
-amazon_top_100_df.columns = ['AMZN_RANK', 'TITLE', 'AUTHOR', 'AMZN_RATING', 'AMZN_REVIEW_COUNT', 'TYPE', 'AMZN_PRICE']
+amazon_top_100_df = amazon_top_100_df.drop(columns=['rank2', 'type2', 'price2'])    
+# Rename columns from (['rank', 'book', 'author', 'rating', 'reviews'], dtype='object')
+amazon_top_100_df.columns = ['AMZN_RANK', 'TITLE', 'AUTHOR', 'AMZN_RATING', 'AMZN_REVIEW_COUNT']
 
 # several cells are blank, replaced nan with empty strings in TITLE and AUTHOR
 amazon_top_100_df[['TITLE', 'AUTHOR']] = amazon_top_100_df[['TITLE', 'AUTHOR']].fillna("")
