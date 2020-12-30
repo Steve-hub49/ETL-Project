54b3fd7ecb6232c50ade0a587bf04dfd62778b6a
Steve Freeland <stevefreeland1@gmail.com>
2020-12-30 17:15:57 -0500

    remove inaccurate comment

--- amazon.py
+++ amazon.py
@@ -2,7 +2,6 @@ import pandas as pd
 
 amazon_top_100_df = pd.read_csv("top_100_books_amazon.csv", encoding='latin1')
 amazon_top_100_df = amazon_top_100_df.drop(columns=['rank2', 'type2', 'price2'])    
-# Rename columns from (['rank', 'book', 'author', 'rating', 'reviews'], dtype='object')
 amazon_top_100_df.columns = ['AMZN_RANK', 'TITLE', 'AUTHOR', 'AMZN_RATING', 'AMZN_REVIEW_COUNT']
 
 # several cells are blank, replaced nan with empty strings in TITLE and AUTHOR
